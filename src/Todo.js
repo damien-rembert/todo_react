3d34d5011232a0763995728f19e18fe0273afe86
Damien R <damien.rembert@gmail.com>
2022-02-09 18:54:36 +0000

    tasks can be added removed marked as done

--- src/Todo.js
+++ src/Todo.js
@@ -4,23 +4,39 @@ import React, { useState } from 'react';
 
 
 
-// task buttons: tick/untick button delete archive buttons
-// bin button: remove task
-// div class=task class=done/todo
+// task buttons: 
+//      tick/untick button => div class=done/todo
+//      delete (bin) onClick task is deleted from todo
+//      archive (folder) onClick task is added to archive and deleted from todo
+//      when in archive button is back to todo and reverts the operation
+ 
+// CSS div class=task class=done/todo
 
-// task object? status: bool, content: string, 
-// array todo / archive
+// task object? status: bool, content: string,
+// 2 arrays: todo / archive
+
+// add task when pressing enter
+
 // grey out add task button when text field empty
+// see disabled in https://www.w3schools.com/html/html_form_input_types.asp
+// and https://www.w3schools.com/html/html_form_attributes.asp
+// do not accept 
 // empty field when add task button pressed
 
 // STRETCH
-// archive all ticked tasks
+// archive all done tasks
+// move done tasks to the bottom of the list
 
 
 
 let Tasks = () => {
 
-    const [taskList, setTaskList] = useState(["task1", "task2", "task3"]);
+    // task object? status: bool, content: string,
+    let task1 = {done: false, content: "I'm task 1"};
+    let task2 = {done: false, content: "I'm task 2"};
+    let task3 = {done: false, content: "I'm task 3"};
+
+    const [taskList, setTaskList] = useState([task1, task2, task3]);
     const [inputText, setInputText] = useState("");
 
 
@@ -29,13 +45,28 @@ let Tasks = () => {
         setInputText(event.target.value);
     }
 
+    const markDone = (index) => {
+        console.log("click");
+        // see checked https://www.w3schools.com/html/html_form_input_types.asp
+        // and https://www.w3schools.com/html/html_form_attributes.asp
+        let storedTasks = [...taskList];
+        let oldTask = storedTasks.splice(index, 1)[0];
+        let newTask = {done: (!oldTask.done), content: oldTask.content};
+        storedTasks.push(newTask);
+        setTaskList(storedTasks);   
+        
+    }
+
     const addHandler = () => {
         // create a new array as the actual one cannot be edited directly
-        let storedTasks = [...taskList];
-        storedTasks.push(inputText);
-        setInputText('');
-        // replace the state array with the edited version 
-        setTaskList(storedTasks);
+        if (inputText) {
+            let storedTasks = [...taskList];
+            let newTask = {done: false, content: inputText};
+            storedTasks.push(newTask);
+            setInputText('');
+            // replace the state array with the edited version 
+            setTaskList(storedTasks);            
+        } 
     }
 
     const removeHandler = (index) => {
@@ -44,19 +75,6 @@ let Tasks = () => {
         setTaskList(storedTasks);
     }
 
-    // use input text
-    // const [inputText, setInputText] = useState("");
-    // const handler = (event) => {
-    //     setInputText(event.target.value);
-    // }
-
-    // return (
-    //     <div>
-    //         <h1>user input</h1>
-    //         <input onChange={handler} />
-    //         <h2>{inputText}</h2>
-    //     </div>
-    // )
 
 
 
@@ -70,7 +88,14 @@ let Tasks = () => {
             {taskList.map((task, index) => {
                 // here we pass the index to that function using anonymous function
                 // return <h1 onClick={() => removeHandler(index)} key={index}>{number}</h1>
-                return <h1 key={index} onClick={() => removeHandler(index)}>{task}</h1>
+                return (
+                    <div key={index} className="task" className={task.done.toString()}>
+                        <input type="checkbox" checked={task.done} onChange={() => markDone(index)}></input>
+                        <p onClick={() => removeHandler(index)}>{task.content}</p>
+                    </div>
+
+                )
+                
             })}
         </div>
     );

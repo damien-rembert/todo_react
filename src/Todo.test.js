8e209fca2283c1f8e23c3688ec6b6710fcc75964
Damien R <damien.rembert@gmail.com>
2022-02-13 13:31:58 +0000

    tests not working getRootNode is not a function

--- src/Todo.test.js
+++ src/Todo.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Todo from './Todo';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Todo />, div);
+});
+
+
+test('renders header', () => {
+  render(<Todo />);
+  const element = screen.getByRole("textbox");
+  expect(element).toBeInTheDocument();
 });

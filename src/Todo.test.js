fcb9423857752f17b3779f0d6cb89b30ab717e4b
Damien Rembert <damien.rembert@gmail.com>
2022-02-13 19:17:29 +0000

    tests are working

--- src/Todo.test.js
+++ src/Todo.test.js
@@ -1,8 +1,129 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Todo from './Todo';
+// import {screen} from '@testing-library/dom'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Todo />, div);
+
+import '@testing-library/jest-dom';
+import { render, screen, fireEvent } from '@testing-library/react';
+// import { render } from '@testing-library/react';
+
+
+// h1 Todo List
+// button add task
+// button Switch mode
+
+
+describe("initial elements", () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Todo />, div);
+  });
+
+  test('renders title', () => {
+    render(<Todo />);
+    const element = screen.getByText(/Todo list/i);
+    expect(element).toBeInTheDocument();
+  });
+
+  test('renders "add task" button', () => {
+    render(<Todo />);
+    const element = screen.getByText(/add task/i);
+    expect(element).toBeInTheDocument();
+  });
+
+  test('renders "switch mode" button', () => {
+    render(<Todo />);
+    const element = screen.getByText(/switch mode/i);
+    expect(element).toBeInTheDocument();
+  });
+
+  test('renders text box', () => {
+    render(<Todo />);
+    const element = screen.getByRole("textbox");
+    expect(element).toBeInTheDocument();
+  });
+
+  test('renders buttons', () => {
+    render(<Todo />);
+    const element = screen.getAllByRole("button");
+    expect(element.length).toBe(2)
+  });
 });
+
+describe("text input", () => {
+  test("input element is in the document", () => {
+    render(<Todo />);
+    const input = screen.getByRole("textbox");
+    expect(input).toBeInTheDocument()
+  })
+      
+  test("input element value changes with new input", () => {
+    render(<Todo />);
+    const input = screen.getByRole("textbox");
+    fireEvent.change(input, { target: { value: "here is a task"} });
+    expect(input.value).toBe("here is a task");
+  })
+
+  test("input is added to the document after button press", () => {
+    render(<Todo />);
+    const input = screen.getByRole("textbox");
+    fireEvent.change(input, { target: { value: "here is a task"} });
+    const button = screen.getByText(/add task/i);
+    fireEvent.click(button);
+    const element = screen.getByText(/here is a task/i);
+    expect(element).toBeInTheDocument();
+  })
+
+  test("input element is empty after adding task", () => {
+    render(<Todo />);
+    const input = screen.getByRole("textbox");
+    fireEvent.change(input, { target: { value: "here is a task"} });
+    const button = screen.getByText(/add task/i);
+    fireEvent.click(button);
+    expect(input.value).toBe("");
+  })
+})
+
+describe("after adding one task", () => {
+  test("delete button is added to the document", () => {
+    render(<Todo />);
+    const input = screen.getByRole("textbox");
+    fireEvent.change(input, { target: { value: "here is a task"} });
+    const button = screen.getByText(/add task/i);
+    fireEvent.click(button);
+    const element = screen.getByText(/delete/i);
+    expect(element).toBeInTheDocument();
+  })
+
+  test("archive button is added to the document", () => {
+    render(<Todo />);
+    const input = screen.getByRole("textbox");
+    fireEvent.change(input, { target: { value: "here is a task"} });
+    const button = screen.getByText(/add task/i);
+    fireEvent.click(button);
+    const element = screen.getByText(/archive/i);
+    expect(element).toBeInTheDocument();
+  })
+
+  test("edit button is added to the document", () => {
+    render(<Todo />);
+    const input = screen.getByRole("textbox");
+    fireEvent.change(input, { target: { value: "here is a task"} });
+    const button = screen.getByText(/add task/i);
+    fireEvent.click(button);
+    const element = screen.getByText(/edit/i);
+    expect(element).toBeInTheDocument();
+  })
+
+  test('renders buttons', () => {
+    render(<Todo />);
+    const input = screen.getByRole("textbox");
+    fireEvent.change(input, { target: { value: "here is a task"} });
+    const button = screen.getByText(/add task/i);
+    fireEvent.click(button);
+    const element = screen.getAllByRole("button");
+    expect(element.length).toBe(5)
+  });
+})

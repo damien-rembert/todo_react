2f09764ab2c643dc08fca4deff52770100c85ea6
Damien Rembert <damien.rembert@gmail.com>
2022-02-15 22:32:03 +0000

    Cleaning - adding readme - adding page title

--- src/Todo.js
+++ src/Todo.js
@@ -2,42 +2,9 @@ import React, { useState } from 'react';
 import './Todo.css';
 
 
-// TODO
-// make the meaning of "switch mode" obvious
-// CSS add a bit of padding
-// grey out add task button when text field empty
-    // see disabled in https://www.w3schools.com/html/html_form_input_types.asp
-    // and https://www.w3schools.com/html/html_form_attributes.asp
-// archive all done tasks
-// if a task will be more than x characters the form should say it's too long
-// add a timestamp to task objects
-// sort by completed then time added https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
-
-// DONE
-// when pressing enter in text field, add task
-// add text for buttons
-// CSS if a task is very long the text should wrap
-// CSS checkbox and buttons should be sitting in squares and stay at the end of their side
-// 2 arrays: todo / archive
-// archive (folder) onClick task is added to archive and deleted from todo
-// edit item click on pen button removes task but send text to the input field
-
-
-
 
 let Tasks = () => {
 
-    // task object? status: bool, content: string,
-
-
-    // let task1 = {done: false, content: "I'm task 1"};
-    // let task2 = {done: false, content: "I'm task 2"};
-    // let task3 = {done: false, content: "I'm task 3"};
-    // let atask1 = {done: false, content: "I'm atask 1"};
-    // let atask2 = {done: false, content: "I'm atask 2"};
-    // let atask3 = {done: false, content: "I'm taask 3"};
-    // const [taskList, setTaskList] = useState([task1, task2, task3]);
-    // const [archiveList, setArchiveList] = useState([atask1, atask2, atask3]);
 
     const [taskList, setTaskList] = useState([]);
     const [archiveList, setArchiveList] = useState([]);
--- src/index.js
+++ src/index.js
@@ -9,7 +9,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root')
-// );
